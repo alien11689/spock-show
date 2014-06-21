cc2a18d97c6e005157e375d39cf23f5f2fce5940
Dominik Adam Przybysz <alien11689@gmail.com>
2014-06-21 10:03:49 +0200

    Add should replacement test

---
+++ src/test/groovy/com/blogspot/przybyszd/spock/extra09/ShouldReplacemetTest.groovy
@@ -0,0 +1,24 @@
+package com.blogspot.przybyszd.spock.extra09
+
+import com.blogspot.przybyszd.spock.dto.Person
+import spock.lang.Specification
+import java.lang.Void as Should
+
+
+class ShouldReplacemetTest extends Specification {
+
+    void "should set first name from constructor"() {
+        when:
+            Person person = new Person(firstName: "Bob")
+        then:
+            person.firstName == "Bob"
+    }
+
+    Should "set first name from constructor"() {
+        when:
+            Person person = new Person(firstName: "Bob")
+        then:
+            person.firstName == "Bob"
+    }
+
+}
