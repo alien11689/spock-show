cd1c4bc26193881cee68d4cb96ae3493d1656155
Dominik Adam Przybysz <alien11689@gmail.com>
2015-06-14 13:11:59 +0200

    Add old example

---
+++ src/test/groovy/com/blogspot/przybyszd/spock/old/OldTest.groovy
@@ -0,0 +1,15 @@
+package com.blogspot.przybyszd.spock.old
+
+import spock.lang.Specification
+
+class OldTest extends Specification {
+    def "should add numbers"() {
+        given:
+            int sum = 6
+            int a = 4
+        when:
+            sum += a
+        then:
+            sum == old(sum) + 4
+    }
+}
