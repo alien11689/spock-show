f5863a5b3ecf87d8782644303c085e8c59e15d65
Dominik Adam Przybysz <alien11689@gmail.com>
2015-06-14 13:08:30 +0200

    Add polling example

---
+++ src/test/groovy/com/blogspot/przybyszd/spock/async10/PoolingConditionTest.groovy
@@ -0,0 +1,25 @@
+package com.blogspot.przybyszd.spock.async10
+
+import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+
+class PoolingConditionTest extends Specification {
+
+    def "should wait for job done"() {
+        given:
+            PollingConditions pollingConditions = new PollingConditions(timeout: 5)
+            String response = null
+        when:
+            new Thread(new Runnable() {
+                @Override
+                void run() {
+                    Thread.sleep(1000)
+                    response = '42'
+                }
+            }).start()
+        then:
+            pollingConditions.eventually {
+                response == '42'
+            }
+    }
+}
